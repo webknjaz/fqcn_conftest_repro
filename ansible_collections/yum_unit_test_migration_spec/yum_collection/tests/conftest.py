d6d6a512d327fe094462edf2ba1825ce341535da
Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>
2019-09-11 18:18:23 +0200

    Drop the conftest hack

--- ansible_collections/yum_unit_test_migration_spec/yum_collection/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Configuration to allow FQCN imports in conftest modules."""
-
-import sys
-
-from ansible.utils.collection_loader import AnsibleCollectionLoader
-
-
-sys.meta_path.insert(0, AnsibleCollectionLoader())
